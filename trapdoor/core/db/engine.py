267e9c06c97327a2a67cd192a3902e77ed6f1d8e
Stijn Van Campenhout <subutux@gmail.com>
2016-11-26 23:21:38 +0100

    Added DB Engine class + PEP8 lint

--- trapdoor/core/db/engine.py
+++ trapdoor/core/db/engine.py
@@ -1,27 +1,55 @@
+import asyncio
 from . import models
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import aiomysql.sa
 import logging
 log = logging.getLogger('trapdoor.core.db.engine')
 log.addHandler(logging.NullHandler())
 
-def get_db_engine(host,user,password,db):
+
+def get_db_engine(host, user, password, db):
     url = "mysql+pymysql://{user}:{password}@{host}/{db}"
-    
+
     log.debug("Creating connection to {}".format(
         url.format(
-        user=user, password="*********", host=host, db=db)))
+            user=user, password="*********", host=host, db=db)))
     url = url.format(
         user=user, password=password, host=host, db=db)
     engine = create_engine(url)
     return engine
+
 
 def get_db_session(engine):
     log.debug("initialize new db session")
     Session = sessionmaker(bind=engine)
     return Session()
 
+
 def init_db(engine):
     log.debug("Creating tables")
     models.Base.metadata.create_all(engine)
 
+
+class Engine(object):
+    """
+    The async DB Engine
+    """
+
+    def __init__(self, config):
+        self._config = config
+        self._engine = None
+        self._session = None
+
+    @asyncio.coroutine
+    def connect(self):
+        self._engine = yield from aiomysql.create_engine(
+            user=self._config["db"]["user"],
+            password=self._config["db"]["host"],
+            host=self._config["db"]["host"],
+            database=self._config["db"]["db"])
+
+    def session(self):
+        if self._session is None:
+            self._session = sessionmaker(bind=self._engine)
+        return self._session
--- trapdoor/core/web.py
+++ trapdoor/core/web.py
@@ -1,44 +1,50 @@
 import asyncio
 from aiohttp import web
+from trapdoor.core.db import Engine
 
 import logging
 log = logging.getLogger('trapdoor.core.web')
 log_access = logging.getLogger('trapdoor.core.web.access')
 log.addHandler(logging.NullHandler())
 
+
 class Web(object):
-	def __init__(self,config,loop=None):
-		if loop == None:
-			self.loop = asyncio.get_event_loop()
-		else:
-			self.loop = loop
+    def __init__(self, config, loop=None):
+        if loop is None:
+            self.loop = asyncio.get_event_loop()
+        else:
+            self.loop = loop
 
-		self._config = config
-		self.app = None
-		self.handler = None
-	@asyncio.coroutine
-	def handle(self,request):
-	    name = request.match_info.get('name', "Anonymous")
-	    text = "Hello, " + name
-	    return web.Response(text=text)
+        self._config = config
+        self.app = None
+        self.handler = None
 
-	@asyncio.coroutine
-	def start(self):
-		port = self._config["web"]["transport"]["ipv4"]["port"]
-		listen = self._config["web"]["transport"]["ipv4"]["listen"]
-		
-		log.info("Launching web on {}:{}".format(listen,port))
+    @asyncio.coroutine
+    def handle(self, request):
+        name = request.match_info.get('name', "Anonymous")
+        text = "Hello, " + name
+        return web.Response(text=text)
 
-		self.app = web.Application(loop=self.loop)
-		self.app.router.add_get('/', self.handle)
-		self.app.router.add_get('/{name}', self.handle)
-		self.handler = self.app.make_handler(logger=log, access_log=log_access)
-		self._server = self.loop.create_server(self.handler, listen, port)
-		#srv = loop.run_until_complete(f)
-		yield from self._server
-		#web.run_app(app,port=port)
-	@asyncio.coroutine
-	def stop(self):
-		log.warning("Stopping Web")
-		yield from self.app.shutdown()
-		yield from self.app.cleanup()
+    @asyncio.coroutine
+    def start(self):
+        port = self._config["web"]["transport"]["ipv4"]["port"]
+        listen = self._config["web"]["transport"]["ipv4"]["listen"]
+
+        log.info("Launching web on {}:{}".format(listen, port))
+
+        self.app = web.Application(loop=self.loop)
+        self.app.db = Engine(self._config)
+        yield from self.app.db.connect()
+        self.app.router.add_get('/', self.handle)
+        self.app.router.add_get('/{name}', self.handle)
+        self.handler = self.app.make_handler(logger=log, access_log=log_access)
+        self._server = self.loop.create_server(self.handler, listen, port)
+        # srv = loop.run_until_complete(f)
+        yield from self._server
+        # web.run_app(app,port=port)
+
+    @asyncio.coroutine
+    def stop(self):
+        log.warning("Stopping Web")
+        yield from self.app.shutdown()
+        yield from self.app.cleanup()
