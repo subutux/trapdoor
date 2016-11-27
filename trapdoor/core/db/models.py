6f2424e867e96d595b87bfcb77880cd3c71f47f4
Stijn Van Campenhout <stijn.vancampenhout@hestia-it.be>
2016-11-27 14:08:10 +0100

    Fixed PEP8 errors + use table name for foreign key instead of class

--- trapdoor/core/db/models.py
+++ trapdoor/core/db/models.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
-from sqlalchemy import Table, Column, String, Integer, ForeignKey, Boolean, DateTime
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import ForeignKey, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 import logging
@@ -11,28 +12,28 @@ Base = declarative_base()
 
 class Permission(Base):
     __tablename__ = "permission"
-    id = Column(Integer, primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     users = relationship("User", cascade="all, delete-orphan",
-                            backref='permission')
-    name = Column(String(256),nullable=False)
-    
+                         backref='permission')
+    name = Column(String(256), nullable=False)
+
+
 class User(Base):
     __tablename__ = "user"
-    id = Column(Integer, primary_key=True,autoincrement=True)
-    login = Column(String(256),nullable=False)
-    password = Column(String(256),nullable=False)
-    is_superuser = Column(Boolean,nullable=False,
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    login = Column(String(256), nullable=False)
+    password = Column(String(256), nullable=False)
+    is_superuser = Column(Boolean, nullable=False,
                           server_default='0')
-    disabled = Column(Boolean,nullable=False,
-                          server_default='0')
-    permissions = Column(Integer,ForeignKey('permission.id'),primary_key=True,nullable=True)
+    disabled = Column(Boolean, nullable=False,
+                      server_default='0')
+    permissions = Column(Integer, ForeignKey(
+        'permission.id'), primary_key=True, nullable=True)
 
-    
-    
 
 class Variable(Base):
     __tablename__ = 'variable'
-    id = Column(Integer,primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     var = Column(String(256))
     val = Column(String(256))
     message_id = Column(ForeignKey('message.id'))
@@ -40,26 +41,29 @@ class Variable(Base):
 
 class Message(Base):
     __tablename__ = "message"
-    id = Column(Integer,primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     host_id = Column(ForeignKey('host.id'))
-    timestamp = sa.Column(DateTime,default=sa.func.now())
-    oid = sa.Column('oid',String(256), nullable=False)
-    mib = sa.Column('mib',String(256), nullable=False)
+    timestamp = sa.Column(DateTime, default=sa.func.now())
+    oid = sa.Column('oid', String(256), nullable=False)
+    mib = sa.Column('mib', String(256), nullable=False)
     variables = relationship(Variable)
+
 
 class Host(Base):
     __tablename__ = 'host'
-    id = Column(Integer, primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     messages = relationship(Message)
-    hostname = sa.Column(String(256),nullable=False)
-    ip = sa.Column(String(256),nullable=False)
-    created = sa.Column(DateTime,default=sa.func.now())
-    active = sa.Column(Boolean,nullable=False,server_default="0")
+    hostname = sa.Column(String(256), nullable=False)
+    ip = sa.Column(String(256), nullable=False)
+    created = sa.Column(DateTime, default=sa.func.now())
+    active = sa.Column(Boolean, nullable=False, server_default="0")
+
 
 class Filter(Base):
     __tablename__ = 'filter'
-    id = Column(Integer, primary_key=True,autoincrement=True)
-    oid = Column(String(256),nullable=True)
-    host = Column(Integer,ForeignKey('Host.id'),primary_key=True,nullable=True)
-    filter = Column(String(256),nullable=False)
-    active = Column(Boolean,nullable=False,server_default='1')
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    oid = Column(String(256), nullable=True)
+    host = Column(Integer, ForeignKey('host.id'),
+                  primary_key=True, nullable=True)
+    filter = Column(String(256), nullable=False)
+    active = Column(Boolean, nullable=False, server_default='1')
